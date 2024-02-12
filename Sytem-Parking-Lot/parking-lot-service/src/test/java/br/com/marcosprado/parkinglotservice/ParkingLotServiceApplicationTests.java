43f397670ca0dd8aab366b52cde8609b32de31d5
Oldiee_ <marcospradofelix@gmail.com>
2024-02-11 22:31:16 -0300

    init service project

---
+++ Sytem-Parking-Lot/parking-lot-service/src/test/java/br/com/marcosprado/parkinglotservice/ParkingLotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.parkinglotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
